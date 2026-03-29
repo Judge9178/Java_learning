30d6e7fdf7ded8cf1b6ba0fec92d24665f6d2f4d
Judge9178 <weijunxing606@gmail.com>
2026-03-29 23:05:33 +0800

    beginning

--- Test.java
+++ Test.java
@@ -1,8 +1,13 @@
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-      Scanner sc=new Scanner(System.in);
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("");
+       String name = input.next();
+        
+   }
       
     
-}//one more push
+}//this is the beginning of the system....

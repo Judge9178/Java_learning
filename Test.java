304082ece2fcd743f22ef773d6e65ce885b40659
Judge9178 <weijunxing606@gmail.com>
2026-03-30 23:07:29 +0800

    day1

--- Test.java
+++ Test.java
@@ -5,8 +5,8 @@ public class Test {
    public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
       System.out.println("用户登录系统");
-
-      
+      System.out.println("-------------------\n");
+      System.out.println("请输入用户名：");
        String name = input.next();
         
    }

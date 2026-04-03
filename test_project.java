9bbcb93c52de534a5472113de455a46deaadaaf9
Judge9178 <weijunxing606@gmail.com>
2026-04-03 12:36:50 +0800

    fix: force lowercase extension

---
+++ test_project.java
@@ -0,0 +1,20 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+public class test_project{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Double a=input.nextDouble();
+        System.out.println("输入的内容是："+a);
+        
+    }
+}
+
+
+
+
+
+
+
+//this is a testing project for my java learning, it is for testing.
+//why pushing is so hard?
+//i hate this.

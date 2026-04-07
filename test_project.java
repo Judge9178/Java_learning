c221c6e2e7c9b6e7218f92eecd620d1affd3fd34
Judge9178 <weijunxing606@gmail.com>
2026-04-07 22:50:28 +0800

    new_day

--- test_project.java
+++ test_project.java
@@ -1,21 +1,9 @@
-import java.io.PrintStream;
-import java.util.Scanner;
-public class test_project{
+import java.util.*;
+
+public class RedPacket {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Double a=input.nextDouble();
-        System.out.println("输入的内容是："+a);
+        Scanner sc = new Scanner(System.in);
         
+       
     }
 }
-
-
-
-
-
-
-
-//this is a testing project for my java learning, it is for testing.
-//why pushing is so hard?
-//i hate this.
-//testing on my laptop.
